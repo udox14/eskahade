c712731c6f0caee1efbd81c0a8e7ed74cf30dff8
M. Iqbal Abdul Wakil <markopolo2212@gmail.comp>
2026-03-08 06:54:57 +0700

    hotfix

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,24 +1,6 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      // Aktifkan KV caching untuk incremental static regeneration
-      incrementalCache: "kv-fetch",
-      tagCache: "cloudflare-kv",
-      queue: "dummy",
-    },
-  },
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-};
-
-export default config;
+export default defineCloudflareConfig({
+  incrementalCache: kvIncrementalCache,
+});
