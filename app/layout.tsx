7b3a3b06a8fd56e60810c11ad760893d2ae137a3
M. Iqbal Abdul Wakil <markopolo2212@gmail.com>
2026-04-10 13:54:43 +0700

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "ESKAHADE",
@@ -45,7 +49,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="id">
+    <html lang="id" className={cn("font-sans", geist.variable)}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-// Fungsi ini berguna untuk menggabungkan class Tailwind secara dinamis
-// Contoh: cn("bg-red-500", kondisi && "text-white")
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
