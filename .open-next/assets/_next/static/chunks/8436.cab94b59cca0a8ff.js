e47f727544bd9aa5dabeffc55fd8bee4e6b1a4e1
udox14 <markopolo2212@gmail.com>
2026-07-18 17:15:41 +0700

    add features

--- .open-next/assets/_next/static/chunks/8436.cab94b59cca0a8ff.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8436],{32383:()=>{},83686:()=>{}}]);
--- .open-next/server-functions/default/.next/server/chunks/ssr/app_b9b1292a._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[26758,a=>{a.v("/_next/static/media/favicon.8cc2386b.ico")},38872,a=>{"use strict";let b={src:a.i(26758).default,width:100,height:87};a.s(["default",0,b])}];
-
-//# sourceMappingURL=app_b9b1292a._.js.map
